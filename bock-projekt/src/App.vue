f51b058a6aab3d58d6137ed4e247de162fb62c2d
LauraDannenbergOpSe2 <laura.dannenberg@stud.hs-bochum.de>
2024-05-27 14:52:14 +0200

    Landing Page
    Ich habe die Landing Page hinzugefügt.
    
    (Logik fehlt: Dass zuerst nur Landing Page und nach Button Klick die Registrierung angezeigt wird, wodurch sich die beiden noch überschneiden)

--- bock-projekt/src/App.vue
+++ bock-projekt/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
+    <Startseite/>
     <RegistrationForm/>
   </div>
 </template>
 
 <script>
 import RegistrationForm from './components/RegistrationsSeite.vue';
+import Startseite from './components/LandingPage.vue';
 
 export default {
   name: 'App',
   components: {
+    Startseite,
     RegistrationForm
   }
 };
---
+++ bock-projekt/src/components/LandingPage.vue
@@ -0,0 +1,38 @@
+<script setup>
+</script>
+
+<template>
+  <div class="Startseite">
+    <div id="alles">
+      <img class="logo" src="../assets/logo3.png" v-on:click="prevent" />
+      
+    </div>
+  </div>
+</template>
+
+<style>
+  :root{
+      --primary-color: #00c853!important;
+      --secondary-color: #d786d8!important;
+      --tertiary-color: #9a46b4!important;
+      --success-color: #5943a8!important;
+      --warning-color: #eaca44!important;
+      --error-color: #ef4d4d!important;
+    }
+
+    /* Theming */
+    #alles{
+      background-color: var(--primary-color);
+      align-items: center;
+      height:100vh;
+    }
+
+    .logo{
+      position: absolute;
+      top: 30vh; /* Anpassen der vertikalen Position nach Bedarf */
+      left: 80vh; /* Anpassen der horizontalen Position nach Bedarf */
+      max-width:70vh;
+      height: auto;
+    }
+
+</style>
