0c209f25e8d675ff307f23bdf77b2177a47fd825
LanceAkaLance <lancekloidt@gmail.com>
2024-06-11 13:22:00 +0200

    Placeholder für Discovery

--- bock-projekt/src/components/SuchSeite.vue
+++ bock-projekt/src/components/SuchSeite.vue
@@ -5,11 +5,17 @@ export default {
   name: 'SuchSeite',
   data:()=>({
     nutzer: [
-        {id: 1, name: "Mark Fischbach", alter: 34, fachbereich: "Yes",hobbies:["Youtube","FNAF","Linsen"],beschreibung: "Hello everybody, my name is Markiplier and welcome to Five Nights at Freddy's, an indie horror game that you guys suggested, en masse, and I saw that Yamimash played it and he said it was really really good... So I'm very eager to see what is up. And that is a terrifying animatronic bear!"},
-        {id: 2, name: "Albert Einstein", alter: "100+", fachbereich: "Irgendwas Schlaues",hobbies:["Denken","Mathe","Physik"],beschreibung: "Ich bin der echte Albert, gib mir deine Kreditkarteninformationen."},
-        {id: 2, name: "Dieter Bohlen", alter: "100+", fachbereich: '"Musik"',hobbies:["Coinmaster"],beschreibung: "Von Bohlen empfohlen."},
-        {id: 2, name: "Max Mustermann", alter: "21", fachbereich: 'Informatik und Elektrotechnik',hobbies:["Yoga","Karate","Gitarre"],beschreibung: "Hey ich spiele übrigens Gitarre (aber nur Wonderwall)"},
-        {id: 2, name: "Pauline Musterfrau", alter: "20", fachbereich: 'Informatik und Elektrotechnik',hobbies:["Webdevelopment","Kickboxen","Häkeln"],beschreibung: "Wenn ich Webseiten baue brauchen die unnedingt ganz viel Klickibunti"}
+    {id: 2, name: "Max Mustermann", alter: "21", fachbereich: 'Informatik und Elektrotechnik',hobbies:["Yoga","Karate","Gitarre"],beschreibung: "Hey ich spiele übrigens Gitarre (aber nur Wonderwall)"},    
+    {id: 2, name: "Pauline Musterfrau", alter: "20", fachbereich: 'Informatik und Elektrotechnik',hobbies:["Webdevelopment","Kickboxen","Häkeln"],beschreibung: "Wenn ich Webseiten baue brauchen die unnedingt ganz viel Klickibunti"},
+    {id: 2, name: "Chris Musterperson", alter: "20", fachbereich: 'Architektur',hobbies:["Lego","Minecraft","Züge"],beschreibung: "Ich mag Züge."},
+    {id: 2, name: "Dieter Bohlen", alter: "100+", fachbereich: '"Musik"',hobbies:["Coinmaster"],beschreibung: "Von Bohlen empfohlen."},
+    {id: 1, name: "Mark Fischbach", alter: 34, fachbereich: "Yes",hobbies:["Youtube","FNAF","Linsen"],beschreibung: "Hello everybody, my name is Markiplier and welcome to Five Nights at Freddy's, an indie horror game that you guys suggested, en masse, and I saw that Yamimash played it and he said it was really really good... So I'm very eager to see what is up. And that is a terrifying animatronic bear!"},
+       
+    {id: 2, name: "Albert Einstein", alter: "100+", fachbereich: "Irgendwas Schlaues",hobbies:["Denken","Mathe","Physik"],beschreibung: "Ich bin der echte Albert, gib mir deine Kreditkarteninformationen."},
+        
+        
+        
+        
     ]
 
   })
