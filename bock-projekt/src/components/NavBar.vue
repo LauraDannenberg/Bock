f522483a99b90da20bb248229dd887fd63fd9575
LanceAkaLance <lancekloidt@gmail.com>
2024-06-12 15:06:03 +0200

    navbar fix profil bearbeiten

--- bock-projekt/src/components/NavBar.vue
+++ bock-projekt/src/components/NavBar.vue
@@ -9,7 +9,7 @@ const route = ref(useRoute())
       <router-link to="/App/Boards" class="noHL" :class="{highlighted:route.name=='Boards - Hobby' || route.name=='Boards - Fachbereich'}"><img src="../assets/Boards.png" alt="Boards" ></router-link>
       <router-link to="/App/Search" class="noHL" :class="{highlighted:route.name=='Discovery'}"><img src="../assets/Lupe.png" alt="Search" ></router-link>
       <router-link to="/App/Chats" class="noHL" :class="{highlighted:route.name=='Chats' ||route.name=='Chat'}"><img src="../assets/Chat.png" alt="Chat" ></router-link>
-      <router-link to="/App/Profile" class="noHL" :class="{highlighted:route.name=='Profil'}"><img src="../assets/Männchen.png" alt="Profile" ></router-link>
+      <router-link to="/App/Profile" class="noHL" :class="{highlighted:route.name=='Profil' || 'Profil bearbeiten'}"><img src="../assets/Männchen.png" alt="Profile" ></router-link>
     </nav>
 </template>
   
