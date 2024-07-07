8354211b1a856c874949c5eeec0539f09f14d3db
Justin Zuenkeler <Justin.Zuenkeler@stud.hs-bochum.de>
2024-07-07 12:37:45 +0200

    BoardsErstellen

---
+++ bock-projekt/src/components/FbErstellen.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="Profilerstellen">
+        <div id="content">
+            <form>
+                <div class="form-group">
+                    <input type="text" id="fachbereich" v-model="fachbereich" :placeholder="fb" required />
+                    <textarea id="about" cols="30" rows="4" v-model="textAbout" :placeholder='placeholder' required ></textarea>
+                </div>
+            </form>
+            <router-link to="/App/Boards/Fachbereich">
+                <button type="submit" class="save-button">
+                    Speichern
+                </button>
+            </router-link> 
+        </div>
+ 
+    </div>    
+</template>
+
+<script>
+
+export default {
+  data:() => ({
+    textAbout:"Beschreib was du gemacht hast!",
+    fachbereich:"Fachbereich:",
+  })
+};
+
+</script>
+
+<style>
+#content{
+    display: flex;
+    flex-direction: column; 
+    justify-content: center;
+    align-items: center;
+    height:75vh;
+    background-color: #00c853;
+}
+</style>
