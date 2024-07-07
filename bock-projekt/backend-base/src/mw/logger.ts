0b6fc6c52b9fea5bf98cacc5b1434d5accf646c4
LanceAkaLance <lancekloidt@gmail.com>
2024-07-07 22:06:51 +0200

    Revert "Backendbase"
    This reverts commit 69bfc684ae9fac39b24625fe40d04d05b913dcb0.

--- bock-projekt/backend-base/src/mw/logger.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { getID, writeToDB } from '../util/db';
-
-export async function logRequest(req: Request, res: Response, next: NextFunction) {
-    const data = { url: req.originalUrl, body: req.body, headers: req.rawHeaders };
-    writeToDB("log", await getID("log"), data);
-    next();
-}
--- bock-projekt/backend-base/src/routes/test.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response, NextFunction } from "express";
-
-const router = express.Router();
-
-router.get('/test', (req, res, next) => {
-    res.send("whoop, you're authenticated!");
-});
-
-export default router;
